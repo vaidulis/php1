a37d577f7bf50ecf1a73075928e5f674353a3b4d
vaidulis <ignotas@gmail.com>
2019-02-01 11:08:36 +0200

    9Uzd done

--- Day3/ND_Day3/Uzd_9d3_rombas.php
+++ Day3/ND_Day3/Uzd_9d3_rombas.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "<h1>Uždavinys 9 - </h1><br>";
+echo "<h1>Uždavinys 9 rombas - </h1><br>";
+
+
+for ($y = 1; $y <= 21; $y++){
+    for ($x = 1; $x <= 21; $x++){
+        if(((11-$y)<$x)&&($x<(11+$y))&&($y<=11)){
+            echo "<p style='line-height: 33%; display:inline;'>*</p>";
+        }
+        elseif(($y>11)&&(($y-11)<$x)&&($x<(33-$y))){
+            echo "<p style='line-height: 33%; display:inline;'>*</p>";
+        }
+        else echo "<p style='line-height: 33%; display:inline; color:white;'>*</p>";
+    }
+    echo '<br>';
+}
